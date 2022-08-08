17c55d75419a2e63d77c00989d46abaa0fa45649
Самохин Артём <110122066+Dark-Heat1208@users.noreply.github.com>
2022-08-08 22:37:32 +0700

    002

---
+++ язык002/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("введите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("привет, ");
+Console.WriteLine(username);
+
+Console.Write("введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("привет, ");
+Console.Write(username);
