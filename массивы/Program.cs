c105b1ff6918d872b66373d5ed83feb185c8b747
Самохин Артём <110122066+Dark-Heat1208@users.noreply.github.com>
2022-08-09 01:17:49 +0700

    нахождение максимального чиста

---
+++ массивы/Program.cs
@@ -0,0 +1,22 @@
+﻿int a = 124;
+int b = 3245;
+int c = 5643;
+int d = 342;
+int e = 345;
+int f = 34;
+int g = 24;
+int r = 432;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+if (f > max) max = f;
+if (g > max) max = g;
+if (r > max) max = r;
+
+Console.Write("max = ");
+Console.WriteLine(max);
