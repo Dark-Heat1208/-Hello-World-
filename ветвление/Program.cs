84c83c099d03100372d8056564694050dd0a1c3f
Самохин Артём <110122066+Dark-Heat1208@users.noreply.github.com>
2022-08-09 00:56:16 +0700

    ветвления

---
+++ ветвление/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username == "Даша")
+{
+    Console.WriteLine("привет тётя ");
+}
+else
+{
+   Console.Write("привет:");
+   Console.WriteLine(username); 
+}
+
